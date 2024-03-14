770e651c7be257ab121d5a88a18b358db95d5165
Arturo Rotondo <3749826+arotondo@users.noreply.github.com>
2024-03-14 11:26:34 -0400

    Add NonNullByDefault annotation.

---
+++ lib/src/main/java/com/ziro/espresso/javax/annotation/extensions/NonNullByDefault.java
@@ -0,0 +1,23 @@
+package com.ziro.espresso.javax.annotation.extensions;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierDefault;
+
+@Documented
+@Nonnull
+@TypeQualifierDefault({
+        ElementType.ANNOTATION_TYPE,
+        ElementType.CONSTRUCTOR,
+        ElementType.FIELD,
+        ElementType.METHOD,
+        ElementType.PACKAGE,
+        ElementType.PARAMETER,
+        ElementType.TYPE
+})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NonNullByDefault {
+}
