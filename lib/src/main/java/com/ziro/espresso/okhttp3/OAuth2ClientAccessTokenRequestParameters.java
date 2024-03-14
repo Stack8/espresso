cf5d724feef1136ad3e7121651e0daecabb25aab
Arturo Rotondo <3749826+arotondo@users.noreply.github.com>
2024-03-14 16:46:19 -0400

    Add OkHttpClientFactory

---
+++ lib/src/main/java/com/ziro/espresso/okhttp3/OAuth2ClientAccessTokenRequestParameters.java
@@ -0,0 +1,23 @@
+package com.ziro.espresso.okhttp3;
+
+import com.google.common.base.MoreObjects;
+import com.ziro.espresso.javax.annotation.extensions.NonNullByDefault;
+
+@NonNullByDefault
+public record OAuth2ClientAccessTokenRequestParameters(
+        String tokenUrl,
+        String clientId,
+        String clientSecret,
+        String scope
+) {
+
+    @Override
+    public String toString() {
+        // Intentionally excluded secret
+        return MoreObjects.toStringHelper(this)
+                .add("tokenUrl", tokenUrl)
+                .add("clientId", clientId)
+                .add("scope", scope)
+                .toString();
+    }
+}
