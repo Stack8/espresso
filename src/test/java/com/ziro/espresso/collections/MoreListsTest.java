a0e788d4bd33b0d84e0dcaa58a69d2d1ba887144
Cory Austin <caustin@goziro.com>
2025-05-12 15:42:42 -0700

    troubleshoot

--- src/test/java/com/ziro/espresso/collections/MoreListsTest.java
+++ src/test/java/com/ziro/espresso/collections/MoreListsTest.java
@@ -24,6 +24,7 @@ class MoreListsTest {
         list2.add(2);
         list2.add(3);
         assertListContainsExactlyInAnyOrder(MoreLists.union(lists), 1, 2, 3);
+        System.out.println("TROUBLESHOOOT");
         assertThat(true).isFalse();
     }
 
