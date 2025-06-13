f1ef2c8e7d50fcdf2577e4c77707b7e82d1695b2
Philippe Min <min.philippe@gmail.com>
2025-06-13 11:43:47 -0400

    Create DeprecatedInfo annotation

---
+++ src/main/java/com/ziro/espresso/javax/annotation/extensions/DeprecatedInfo.java
@@ -0,0 +1,45 @@
+package com.ziro.espresso.javax.annotation.extensions;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Provides additional context for deprecated elements.
+ * <p>
+ * This annotation should be used in conjunction with {@code @Deprecated} to provide
+ * information about when the element was deprecated and when it's scheduled for removal.
+ * </p>
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({
+    ElementType.CONSTRUCTOR,
+    ElementType.FIELD,
+    ElementType.LOCAL_VARIABLE,
+    ElementType.METHOD,
+    ElementType.PACKAGE,
+    ElementType.MODULE,
+    ElementType.PARAMETER,
+    ElementType.TYPE
+})
+public @interface DeprecatedInfo {
+
+    /**
+     * Reference to the story/issue where this element was marked as deprecated.
+     * This field is mandatory.
+     *
+     * @return the story/issue reference
+     */
+    String deprecatedIn();
+
+    /**
+     * Reference to the story/issue where this element is scheduled for removal.
+     * This field is mandatory.
+     *
+     * @return the story/issue reference for removal
+     */
+    String removingIn();
+}
